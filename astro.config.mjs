400f6423aec4217a6b5af99d0c78635549516109
MauroTre <m.gutierrez7@hotmail.com>
2024-03-27 23:50:34 -0500

    d

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import reactRefresh from '@vitejs/plugin-react-refresh'; // Agrega este import
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind(),
+  ],
+  plugins: [
+    reactRefresh(), // Agrega este plugin para React
+  ],
+});
