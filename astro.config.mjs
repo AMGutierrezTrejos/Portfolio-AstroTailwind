331123ee4f5fad29498b5695a6fd689f2091ab93
MauroTre <m.gutierrez7@hotmail.com>
2024-04-01 23:05:40 -0500

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import reactRefresh from '@vitejs/plugin-react-refresh'; // Agrega este import
+import react from '@astrojs/react';
+
 
 export default defineConfig({
   integrations: [
     tailwind(),
+    react(),
   ],
   plugins: [
     reactRefresh(), // Agrega este plugin para React
